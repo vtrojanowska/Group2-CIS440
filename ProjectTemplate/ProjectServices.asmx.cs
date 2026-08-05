1c40439f40c7fe7f736a8f85076bd4718d7ed9a0
Alexanderwilson2257 <Alexanderwilson2257@gmail.com>
2026-08-05 14:23:24 -0700

    Update team database connection

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -1,66 +1,196 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Services;
-using MySql.Data;
 using MySql.Data.MySqlClient;
-using System.Data;
 
 namespace ProjectTemplate
 {
-	[WebService(Namespace = "http://tempuri.org/")]
-	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-	[System.ComponentModel.ToolboxItem(false)]
-	[System.Web.Script.Services.ScriptService]
+    public class FeedbackSubmission
+    {
+        public int id { get; set; }
+        public string category { get; set; }
+        public string concern { get; set; }
+        public string solution { get; set; }
+        public string status { get; set; }
+        public string submitted_on { get; set; }
+    }
 
-	public class ProjectServices : System.Web.Services.WebService
-	{
-		////////////////////////////////////////////////////////////////////////
-		///replace the values of these variables with your database credentials
-		////////////////////////////////////////////////////////////////////////
-		private string dbID = "cis440template";
-		private string dbPass = "!!Cis440";
-		private string dbName = "cis440template";
-		////////////////////////////////////////////////////////////////////////
-		
-		////////////////////////////////////////////////////////////////////////
-		///call this method anywhere that you need the connection string!
-		////////////////////////////////////////////////////////////////////////
-		private string getConString() {
-			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
-		}
-		////////////////////////////////////////////////////////////////////////
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
 
+    public class ProjectServices : WebService
+    {
+        private string dbID = "cis440sum26team2";
+        private string dbPass = "cis440sum26team2";
+        private string dbName = "cis440sum26team2";
 
+        private string getConString()
+        {
+            return "SERVER=107.180.1.16; PORT=3306; DATABASE=" +
+                dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
+        }
 
-		/////////////////////////////////////////////////////////////////////////
-		//don't forget to include this decoration above each method that you want
-		//to be exposed as a web service!
-		[WebMethod(EnableSession = true)]
-		/////////////////////////////////////////////////////////////////////////
-		public string TestConnection()
-		{
-			try
-			{
-				string testQuery = "select * from test";
+        [WebMethod(EnableSession = true)]
+        public string TestConnection()
+        {
+            try
+            {
+                using (MySqlConnection con =
+                    new MySqlConnection(getConString()))
+                {
+                    con.Open();
+                }
 
-				////////////////////////////////////////////////////////////////////////
-				///here's an example of using the getConString method!
-				////////////////////////////////////////////////////////////////////////
-				MySqlConnection con = new MySqlConnection(getConString());
-				////////////////////////////////////////////////////////////////////////
+                return "Success!";
+            }
+            catch (Exception e)
+            {
+                return "Something went wrong. Error: " + e.Message;
+            }
+        }
 
-				MySqlCommand cmd = new MySqlCommand(testQuery, con);
-				MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-				DataTable table = new DataTable();
-				adapter.Fill(table);
-				return "Success!";
-			}
-			catch (Exception e)
-			{
-				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
-			}
-		}
-	}
+        [WebMethod(EnableSession = true)]
+        public bool SubmitFeedback(
+            string category,
+            string concern,
+            string solution)
+        {
+            try
+            {
+                string query =
+                    "INSERT INTO submissions " +
+                    "(category, concern, solution, status) " +
+                    "VALUES (@category, @concern, @solution, 'Submitted')";
+
+                using (MySqlConnection con =
+                    new MySqlConnection(getConString()))
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue(
+                        "@category",
+                        category
+                    );
+
+                    cmd.Parameters.AddWithValue(
+                        "@concern",
+                        concern
+                    );
+
+                    cmd.Parameters.AddWithValue(
+                        "@solution",
+                        solution
+                    );
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [WebMethod(EnableSession = true)]
+        public FeedbackSubmission[] GetSubmissions()
+        {
+            List<FeedbackSubmission> submissions =
+                new List<FeedbackSubmission>();
+
+            string query =
+                "SELECT id, category, concern, solution, status, " +
+                "DATE_FORMAT(submitted_on, '%m/%d/%Y') AS submitted_on " +
+                "FROM submissions ORDER BY id DESC";
+
+            using (MySqlConnection con =
+                new MySqlConnection(getConString()))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, con);
+
+                con.Open();
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        FeedbackSubmission submission =
+                            new FeedbackSubmission();
+
+                        submission.id =
+                            Convert.ToInt32(reader["id"]);
+
+                        submission.category =
+                            reader["category"].ToString();
+
+                        submission.concern =
+                            reader["concern"].ToString();
+
+                        submission.solution =
+                            reader["solution"].ToString();
+
+                        submission.status =
+                            reader["status"].ToString();
+
+                        submission.submitted_on =
+                            reader["submitted_on"].ToString();
+
+                        submissions.Add(submission);
+                    }
+                }
+            }
+
+            return submissions.ToArray();
+        }
+
+        [WebMethod(EnableSession = true)]
+        public bool UpdateSubmissionStatus(
+            int id,
+            string status)
+        {
+            string[] allowedStatuses =
+            {
+                "Submitted",
+                "Under Review",
+                "In Progress",
+                "Resolved"
+            };
+
+            if (Array.IndexOf(allowedStatuses, status) < 0)
+            {
+                return false;
+            }
+
+            try
+            {
+                string query =
+                    "UPDATE submissions " +
+                    "SET status = @status " +
+                    "WHERE id = @id";
+
+                using (MySqlConnection con =
+                    new MySqlConnection(getConString()))
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    con.Open();
+
+                    int rowsChanged = cmd.ExecuteNonQuery();
+
+                    return rowsChanged > 0;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
 }
